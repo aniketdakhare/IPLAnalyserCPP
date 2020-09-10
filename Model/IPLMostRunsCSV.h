c2503d92173698f3c4c93fc134faaf610977c23d
“Aniket_D” <aniketdakhare@gmail.com>
2020-09-10 19:59:47 +0530

    ADDED Pojo class for IPL's Batsman Details

---
+++ Model/IPLMostRunsCSV.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class IPLBatsmanData
+{
+    public:
+        string playerName;
+        double average;
+        double strikeRate;
+        int fours;
+        int sixes;
+        int runs;
+
+        IPLBatsmanData(){}
+};
